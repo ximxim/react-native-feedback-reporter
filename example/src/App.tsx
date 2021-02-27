55eafd3d2bafe7685a6a0bf21372eb7e13fac993
ximxim <azim.ahmed7@gmail.com>
2021-02-26 22:20:44 -0500

    chore: init

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import FeedbackReporter from 'react-native-feedback-reporter';
 
-
 export default function App() {
   const [result, setResult] = React.useState<number | undefined>();
 
