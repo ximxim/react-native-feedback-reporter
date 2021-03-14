d113040b5a0f8a5a47644310a0636595acec546b
ximxim <azim.ahmed7@gmail.com>
2021-03-14 00:41:08 -0500

    chore: moved submit JIRA issue logic to its own hook

--- src/integrations/JIRA/useJIRAIntegration.hook.tsx
+++ src/integrations/JIRA/useJIRAIntegration.hook.tsx
@@ -4,24 +4,15 @@ import { useFormContext } from 'react-hook-form';
 
 import { initJIRAApi } from './JIRAApi.service';
 import { ProjectSelector, IssueTypeSelector } from './components';
-import {
-  postJIRAIssue,
-  IPostJIRAIssueResponse,
-  postJIRAIssueAttachents,
-} from './mutations';
+import { useJIRASubmission } from './useJIRASubmission.hook';
 
-import type { IFile } from '../../utils';
 import { GlobalProps, IReportFormValues, Typography } from '../../components';
 
 export const useJIRAIntegration = () => {
-  const [issue, setIssue] = useState<IPostJIRAIssueResponse>();
+  const { issue, submitToJIRA } = useJIRASubmission();
   const [isRegistered, setIsRegistered] = useState<boolean>(false);
-  const { jira, devNotes } = useContext(GlobalProps);
-  const {
-    register,
-    getValues,
-    unregister,
-  } = useFormContext<IReportFormValues>();
+  const { jira } = useContext(GlobalProps);
+  const { register, unregister } = useFormContext<IReportFormValues>();
 
   useEffect(() => {
     if (!jira) return;
@@ -40,46 +31,6 @@ export const useJIRAIntegration = () => {
   }, [register]);
 
   if (!jira) return { JIRAComponents: null, submitToJIRA: () => {} };
-
-  const submitToJIRA = async (files: IFile[]) => {
-    setIssue(undefined);
-
-    const {
-      uri,
-      title,
-      description,
-      JIRAProject: projectId,
-      JIRAIssueType: issueTypeId,
-    } = getValues();
-
-    if (!jira || !projectId || !issueTypeId) return;
-
-    const { username, token, domain } = jira;
-
-    const generatedDevNotes =
-      typeof devNotes === 'string' ? devNotes : await devNotes?.();
-
-    const res = await postJIRAIssue({
-      title,
-      projectId,
-      issueTypeId,
-      description,
-      devNotes: generatedDevNotes,
-    });
-
-    setIssue(res.data);
-
-    if (!res.data.key) return;
-
-    await postJIRAIssueAttachents({
-      files,
-      token,
-      domain,
-      username,
-      content: uri,
-      key: res.data.key,
-    });
-  };
 
   const JIRAComponents = isRegistered && (
     <>
---
+++ src/integrations/JIRA/useJIRASubmission.hook.ts
@@ -0,0 +1,57 @@
+import { useContext, useEffect, useState } from 'react';
+import { useFormContext } from 'react-hook-form';
+import { useMutation } from 'react-query';
+
+import { postJIRAIssue, postJIRAIssueAttachents } from './mutations';
+
+import type { IFile } from '../../utils';
+import { IReportFormValues, GlobalProps } from '../../components';
+
+export const useJIRASubmission = () => {
+  const [files, setFiles] = useState<IFile[]>([]);
+  const { mutate: postIssue, data: issueRes } = useMutation(postJIRAIssue);
+  const { mutate: postIssueAttachments } = useMutation(postJIRAIssueAttachents);
+  const { getValues } = useFormContext<IReportFormValues>();
+  const { jira, devNotes } = useContext(GlobalProps);
+
+  useEffect(() => {
+    // TODO: handle ticket creation failure
+    if (!issueRes?.data.key || !jira) return;
+    const { username, token, domain } = jira;
+    const { uri } = getValues();
+
+    postIssueAttachments({
+      files,
+      token,
+      domain,
+      username,
+      content: uri,
+      key: issueRes.data.key,
+    });
+  }, [issueRes]);
+
+  const submitToJIRA = async (files: IFile[]) => {
+    setFiles(files);
+    const {
+      title,
+      description,
+      JIRAProject: projectId,
+      JIRAIssueType: issueTypeId,
+    } = getValues();
+
+    if (!jira || !projectId || !issueTypeId) return;
+
+    const generatedDevNotes =
+      typeof devNotes === 'string' ? devNotes : await devNotes?.();
+
+    postIssue({
+      title,
+      projectId,
+      issueTypeId,
+      description,
+      devNotes: generatedDevNotes,
+    });
+  };
+
+  return { issue: issueRes?.data, submitToJIRA };
+};
