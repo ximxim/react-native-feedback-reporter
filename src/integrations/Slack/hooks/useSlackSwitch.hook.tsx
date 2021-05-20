6ab4d965b4e47c6214ddcb6f8cd2e25de60db85b
ximxim <azim.ahmed7@gmail.com>
2021-05-20 00:03:46 -0400

    chore: switches for integrations are storing

--- src/integrations/Slack/hooks/useSlackSwitch.hook.tsx
+++ src/integrations/Slack/hooks/useSlackSwitch.hook.tsx
@@ -1,22 +1,37 @@
 import React from 'react';
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { useFormContext } from 'react-hook-form';
 
+import { useStorage } from '../../../hooks';
 import { IReportFormValues, GlobalProps, Switch } from '../../../components';
 
 export const useSlackSwitch = () => {
+  const [isReady, setIsReady] = useState<boolean>(false);
   const { slack } = useContext(GlobalProps);
+  const { setItem, getItem } = useStorage({
+    key: 'FEEDBACK_REPORTER_SLACK_SWITCH',
+  });
   const {
     setValue,
     register,
+    getValues,
     unregister,
   } = useFormContext<IReportFormValues>();
 
   useEffect(() => {
-    if (!slack) return;
+    (async () => {
+      if (!slack) return;
 
-    register('slackSwitch');
-    setValue('slackSwitch', !!slack);
+      register('slackSwitch');
+      try {
+        const value = await getItem();
+        setValue('slackSwitch', value ? value === 'true' : !!slack);
+      } catch (e) {
+        setValue('slackSwitch', !!slack);
+      } finally {
+        setIsReady(true);
+      }
+    })();
 
     return () => {
       unregister(['slackSwitch']);
@@ -24,11 +39,14 @@ export const useSlackSwitch = () => {
   }, [register]);
 
   return (
-    !!slack && (
+    !!slack && isReady && (
       <Switch
-        onChange={(val) => setValue('slackSwitch', val)}
+        onChange={(val) => {
+          setItem(val ? 'true' : 'false');
+          setValue('slackSwitch', val);
+        }}
         label="Enable slack integration"
-        defaultValue={!!slack}
+        defaultValue={getValues('slackSwitch')}
       />
     )
   );
