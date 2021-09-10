18e8fee1b377f7e87d071cbc9f52b92006a5f860
lazerblasters <40807667+lazerblasters@users.noreply.github.com>
2021-09-10 09:44:06 -0400

    chore: adding refs on the fly

--- src/hooks/useNavigation.tsx
+++ src/hooks/useNavigation.tsx
@@ -30,20 +30,29 @@ export const useNavigation = (
   const [pageNumber, setPageNumber] = useState<number>(0);
   const scrollViewRef = useRef<ScrollView>(null);
   const [height, setHeight] = useState<number>();
-  const elRefs = useRef<RefObject<View>[]>(data.map(() => createRef())).current;
+  const elRefs = useRef<RefObject<View>[]>(data.map(() => createRef()));
 
   useEffect(() => {
     LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
   }, [height]);
 
   useEffect(() => {
+    if (data.length == elRefs.current.length) return;
+    data.slice(elRefs.current.length).map(() => {
+      elRefs.current.push(createRef());
+    });
+  }, [data]);
+
+  useEffect(() => {
     (async () => {
       await new Promise((resolve) => setTimeout(resolve, 200));
 
-      elRefs[pageNumber].current?.measureLayout(
+      console.log(elRefs.current[pageNumber], elRefs, pageNumber);
+      elRefs.current[pageNumber].current?.measureLayout(
         // @ts-ignore: ref typescript is weird and old so ignoring it
         scrollViewRef.current,
         (_left, _top, _width, elHeight) => {
+          console.log(_left, _top, _width, elHeight);
           if (elHeight <= 0 || elHeight === height) return;
           setHeight(elHeight);
         },
@@ -65,12 +74,16 @@ export const useNavigation = (
       pagingEnabled
       ref={scrollViewRef}
       style={{ height }}
-      contentContainerStyle={{ height: (height || 0) + 40 }}
+      contentContainerStyle={{
+        height: (height || 0) + 40,
+      }}
       scrollEnabled={false}
       showsHorizontalScrollIndicator={false}
     >
       {data?.map(({ component }, index) => (
-        <View style={{ width }}>{component(elRefs[index], setPageNumber)}</View>
+        <View style={{ width }}>
+          {component(elRefs.current[index], setPageNumber)}
+        </View>
       ))}
     </ScrollView>
   );
