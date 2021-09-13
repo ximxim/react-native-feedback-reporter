a4ea8d2b129e02a65c9038a1b26c38f867f1568c
lazerblasters <40807667+lazerblasters@users.noreply.github.com>
2021-09-12 22:55:39 -0400

    fix: issue with json tree on android

--- src/components/form/ReportForm/components/Preview/Preview.styles.ts
+++ src/components/form/ReportForm/components/Preview/Preview.styles.ts
@@ -13,32 +13,40 @@ export const JSONTree = styled<any>(RNJSONTree).attrs(
   ({ theme }: RNFRThemeType) => ({
     theme: {
       label: {
+        fontFamily: 'System',
         color: theme.colors.brandSecondary,
       },
       valueLabel: {
+        fontFamily: 'System',
         color: theme.colors.brandSecondary,
       },
       value: {
+        fontFamily: 'System',
         color: theme.colors.brandSecondary,
       },
       tree: {
+        fontFamily: 'System',
         backgroundColor: theme.colors.brandPrimary,
       },
       arrowSign: {
+        fontFamily: 'System',
         color: theme.colors.brandSecondary,
       },
       nestedNodeItemType: {
+        fontFamily: 'System',
         color: theme.colors.brandMuted,
       },
       valueText: ({ style }: any) => ({
         style: {
           ...style,
+          fontFamily: 'System',
           color: theme.colors.brandDanger,
         },
       }),
       nestedNodeItemString: ({ style }: any) => ({
         style: {
           ...style,
+          fontFamily: 'System',
           color: theme.colors.brandMuted,
         },
       }),
